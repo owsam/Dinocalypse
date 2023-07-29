547c317da307b4299d27c04135c757838917fbec
Leonid Barsht <leonidb93@gmail.com>
2023-07-29 16:45:45 +0200

    implement movement, jumping, weight stuff

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float weight = 50;
+
+    // Stuff from Unity
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    private Vector2 movementVector = new();
+    private bool jumped;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(movementVector.x, 0, 0);
+        controller.Move(playerSpeed * Time.deltaTime * move);
+
+        // Changes the height position of the player..
+        if (jumped && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight / weight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+        jumped = false;
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        movementVector = movementValue.Get<Vector2>();
+    }
+
+    void OnJump(InputValue movementValue)
+    {
+        jumped = movementValue.isPressed;
+    }
+}
